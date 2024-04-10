980c400003fa37cebf0a8e63205e13175f2fa2ad
Flutter Dev <flutterdev06@gmail.com>
2024-04-09 20:05:08 -0400

    Code refactoring & generate new requirements.txt file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import stag
+import steganography
 
 app = Flask(__name__)
 
@@ -14,14 +14,14 @@ def encode_message():
     if request.method == 'POST':
         image = request.files.get('image')
         message = request.values.get("message")
-        return stag.encode(image, message)
+        return steganography.encode(image, message)
 
 
 @app.route("/decode-message", methods=['POST'])
 def decode_message():
     if request.method == 'POST':
         image = request.files.get('image')
-        return stag.decode(image)
+        return steganography.decode(image)
 
 
 app.run(host="0.0.0.0", port=8000, debug=True)
--- steganography.py
+++ steganography.py
@@ -1,30 +1,25 @@
-# Python program implementing Image Steganography
+import math
 
-# PIL module is used to extract
-# pixels of image and modify it
 from PIL import Image
+import datetime
 
 
-# Convert encoding data into 8-bit binary
-# form using ASCII value of characters
-def genData(data):
+def gen_data(data):
     # list of binary codes
     # of given data
-    newd = []
+    new_data = []
 
     for i in data:
-        newd.append(format(ord(i), '08b'))
-    return newd
+        new_data.append(format(ord(i), '08b'))
+    return new_data
 
 
-# Pixels are modified according to the
-# 8-bit binary data and finally returned
-def modPix(pix, data):
-    datalist = genData(data)
-    lendata = len(datalist)
+def mod_pix(pix, data):
+    data_list = gen_data(data)
+    len_data = len(data_list)
     imdata = iter(pix)
 
-    for i in range(lendata):
+    for i in range(len_data):
 
         # Extracting 3 pixels at a time
         pix = [value for value in imdata.__next__()[:3] +
@@ -34,10 +29,10 @@ def modPix(pix, data):
         # Pixel value should be made
         # odd for 1 and even for 0
         for j in range(0, 8):
-            if datalist[i][j] == '0' and pix[j] % 2 != 0:
+            if data_list[i][j] == '0' and pix[j] % 2 != 0:
                 pix[j] -= 1
 
-            elif datalist[i][j] == '1' and pix[j] % 2 == 0:
+            elif data_list[i][j] == '1' and pix[j] % 2 == 0:
                 if pix[j] != 0:
                     pix[j] -= 1
                 else:
@@ -47,7 +42,7 @@ def modPix(pix, data):
         # whether to stop ot read further.
         # 0 means keep reading; 1 means thec
         # message is over.
-        if i == lendata - 1:
+        if i == len_data - 1:
             if pix[-1] % 2 == 0:
                 if pix[-1] != 0:
                     pix[-1] -= 1
@@ -64,14 +59,14 @@ def modPix(pix, data):
         yield pix[6:9]
 
 
-def encode_enc(newimg, data):
-    w = newimg.size[0]
+def encode_enc(new_img, data):
+    w = new_img.size[0]
     (x, y) = (0, 0)
 
-    for pixel in modPix(newimg.getdata(), data):
+    for pixel in mod_pix(new_img.getdata(), data):
 
         # Putting modified pixels in the new image
-        newimg.putpixel((x, y), pixel)
+        new_img.putpixel((x, y), pixel)
         if x == w - 1:
             x = 0
             y += 1
@@ -79,27 +74,22 @@ def encode_enc(newimg, data):
             x += 1
 
 
-# Encode data into image
-def encode():
-    img = input("Enter image name(with extension) : ")
-    image = Image.open(img, 'r')
-
-    data = input("Enter data to be encoded : ")
-    if len(data) == 0:
+def encode(image, message):
+    if len(message) == 0:
         raise ValueError('Data is empty')
 
-    newimg = image.copy()
-    encode_enc(newimg, data)
+    new_img = Image.open(image)
+    encode_enc(new_img, message + get_timestamp())
+    filename = image.filename.split('.')[0]
+    extension = image.filename.split('.').pop()
+    encoded_filename = filename + '_encoded.' + extension
+    encoded_image_path = f'static/encoded_images/{encoded_filename}'
+    new_img.save(encoded_image_path)
+    return encoded_image_path
 
-    new_img_name = input("Enter the name of new image(with extension) : ")
-    newimg.save(new_img_name, str(new_img_name.split(".")[1].upper()))
 
-
-# Decode the data in the image
-def decode():
-    img = input("Enter image name(with extension) : ")
-    image = Image.open(img, 'r')
-
+def decode(img):
+    image = Image.open(img)
     data = ''
     imgdata = iter(image.getdata())
 
@@ -122,20 +112,7 @@ def decode():
             return data
 
 
-# Main Function
-def main():
-    a = int(input(":: Welcome to Steganography ::\n"
-                  "1. Encode\n2. Decode\n"))
-    if a == 1:
-        encode()
-
-    elif a == 2:
-        print("Decoded Word : " + decode())
-    else:
-        raise Exception("Enter correct input")
-
-
-# Driver Code
-# if __name__ == '__main__':
-#     # Calling main function
-#     main()
+def get_timestamp():
+    # __1712703795__
+    epoch = datetime.datetime.timestamp(datetime.datetime.now())
+    return f"__{math.floor(epoch)}__"
