3c3724d6bb43e0a80a95a92fd51c27180af53f9f
ajaya03 <162769546+ajaya03@users.noreply.github.com>
2024-04-11 13:21:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,3 @@ def decode_message():
     if request.method == 'POST':
         image = request.files.get('image')
         return steganography.decode(image)
-
-
-app.run(host="0.0.0.0", port=8000, debug=True)
