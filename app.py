2e84f7cde7c90045faf435e647fcf92f92a8e3b3
ajaya03 <dhungelajay@gmail.com>
2024-04-13 19:56:44 -0400

    Revert

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import steganography
-
 app = Flask(__name__)
 
 
@@ -17,7 +16,7 @@ def encode_message():
             message = request.values.get("message")
             return steganography.encode(image, message)
     except Exception as e:
-        return jsonify({"success": False, "error": str(e)}), 500    
+        return ({"success": False, "error": str(e)}), 500    
 
     
 @app.route("/decode-message", methods=['POST','GET'])
@@ -25,6 +24,3 @@ def decode_message():
     if request.method == 'POST':
         image = request.files.get('image')
         return steganography.decode(image)
-
-
-
