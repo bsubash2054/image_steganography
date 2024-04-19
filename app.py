8dcf9f7c326ebe750bc7cbbd1f72aa294449de92
Flutter Dev <flutterdev06@gmail.com>
2024-04-19 01:04:57 -0400

    Added  proper comments to code

--- app.py
+++ app.py
@@ -4,21 +4,33 @@ import steganography
 app = Flask(__name__)
 
 
+# Route for displaying encode and decode message forms. This is the default route of the site.
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
+# Route for encoding message in image. Only accepts POST requests.
 @app.route("/encode-message", methods=['POST'])
 def encode_message():
+    """
+    This function handles encoding message inside the image.
+    :return: The path of the encoded image that the users can download.
+    """
     if request.method == 'POST':
         image = request.files.get('image')
         message = request.values.get("message")
         return steganography.encode(image, message)
 
 
+# Route for decoding messages. Only accepts POST requests
 @app.route("/decode-message", methods=['POST'])
 def decode_message():
+    """
+    This function handles the decoding of message from the image.
+    :return: The text that is decoded. This text contains <message> + <timestamp>. These two items are
+    separated by the frontend and displayed accordingly.
+    """
     if request.method == 'POST':
         image = request.files.get('image')
         return steganography.decode(image)
--- steganography.py
+++ steganography.py
@@ -5,8 +5,7 @@ import datetime
 
 
 def gen_data(data):
-    # list of binary codes
-    # of given data
+    # This will hold binary codes of given data
     new_data = []
 
     for i in data:
@@ -21,13 +20,12 @@ def mod_pix(pix, data):
 
     for i in range(len_data):
 
-        # Extracting 3 pixels at a time
+        # Extracting 3 pixels at a time to insert one character
         pix = [value for value in imdata.__next__()[:3] +
                imdata.__next__()[:3] +
                imdata.__next__()[:3]]
 
-        # Pixel value should be made
-        # odd for 1 and even for 0
+        # Pixel value should be made odd for 1 and even for 0
         for j in range(0, 8):
             if data_list[i][j] == '0' and pix[j] % 2 != 0:
                 pix[j] -= 1
@@ -38,10 +36,8 @@ def mod_pix(pix, data):
                 else:
                     pix[j] += 1
 
-        # Eighth pixel of every set tells
-        # whether to stop ot read further.
-        # 0 means keep reading; 1 means the
-        # message is over.
+        # If there is message still to be included, make the ninth value even
+        # If the message is finished, make the ninth value odd indicating end of encoded message.
         if i == len_data - 1:
             if pix[-1] % 2 == 0:
                 if pix[-1] != 0:
@@ -75,16 +71,42 @@ def encode_enc(new_img, data):
 
 
 def encode(image, message):
+    """
+    The top level function that performs encoding of message inside image.
+    :param image: The image file received from the user/browser.
+    :param message: The message text that is to be included inside the image.
+    :return: The path of the encoded image.
+    """
+    # Throw error if the message is empty.
     if len(message) == 0:
         raise ValueError('Data is empty')
 
+    # Load image in memory
     new_img = Image.open(image)
+
+    # Main function for encoding message in image.
+    # We also concatenate the current timestamp along with the message in order to be able to show
+    # date and time when the message was encoded.
     encode_enc(new_img, message + get_timestamp())
+
+    # Getting filename. Taking everything before the last dot (.)
     filename = image.filename.split('.')[0]
+
+    # Finding the extension of the image.
     extension = image.filename.split('.').pop()
+
+    # Providing new name to the file to be saved.
+    # The naming format is: <old-name> + "_encoded".<file-extension>
     encoded_filename = filename + '_encoded.' + extension
+
+    # This is the final path of the image where it will be saved.
     encoded_image_path = f'static/encoded_images/{encoded_filename}'
+
+    # Saving image. After this, users can download this image from the saved path.
     new_img.save(encoded_image_path)
+
+    # We return the path of the encoded image as response so that the frontend can prompt the
+    # user to download the encoded image.
     return encoded_image_path
 
 
@@ -101,6 +123,8 @@ def decode(img):
         # string of binary data
         binstr = ''
 
+        # if value is even, treat it as 0
+        # If value is odd, treat it as 1
         for i in pixels[:8]:
             if i % 2 == 0:
                 binstr += '0'
@@ -108,11 +132,17 @@ def decode(img):
                 binstr += '1'
 
         data += chr(int(binstr, 2))
+
+        # Keep on reading the pixels until the third value of third pixel (i.e. ninth value in group) is odd.
+        # If this value is even, it means message has not been finished
+        # If odd, this indicates the end of encoded message and hence, stop reading further.
         if pixels[-1] % 2 != 0:
             return data
 
 
 def get_timestamp():
-    # __1712703795__
+    # We append the timestamp to the message as: __1712703795__
     epoch = datetime.datetime.timestamp(datetime.datetime.now())
+
+    # getting rid of decimal points.
     return f"__{math.floor(epoch)}__"
